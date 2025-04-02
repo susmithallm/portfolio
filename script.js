129ccfdc03ed462f87abbcae4056e84aaa4fa9e2
susmithallm <kattamurisusmitha@gmail.com>
2025-04-03 00:50:09 +0530

    Add files via upload
    https://1drv.ms/f/c/d2f6d16c64d22051/Enzo4SnQR0VIqofC_NVCnxoBTy0YxmTE-CW1RJPG3_Rl_w?e=CKjIvn

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth Scroll for Navigation Links
+const links = document.querySelectorAll('nav ul li a');
+
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
